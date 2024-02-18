ee2158ae38c2c476a557c55a078906a08c2d0502
Tim Bryant <timothybryant3@gmail.com>
2024-02-18 13:14:59 -0500

    chore: Add local binary path to PATH

--- dot_config/bin/installers/tfenv_terraform.sh
+++ dot_config/bin/installers/tfenv_terraform.sh
@@ -59,6 +59,9 @@ if [[ -z "${TF_VERSION}" ]]; then
     exit_with_error "TF_VERSION is not set. Please export TF_VERSION with the desired Terraform version."
 fi
 
+# Add local binary path to PATH (where unzip is installed which is required by tfenv)
+add_to_path "$HOME/.local/bin"
+
 # Check if Terraform is installed and working
 if ! terraform_version >/dev/null; then
     echo_with_color "$YELLOW_COLOR" "Terraform could not be found or is not properly installed."
