4ce471614850977f804dcb2d64f2f0f3f2cb9b12
Tim Bryant <timothybryant3@gmail.com>
2024-02-19 12:25:01 -0500

    fix: Use sudo when starting qemu-guest-agent to avoid permission issues

--- dot_config/bin/adhoc/executable_qemu_guest_agent.sh
+++ dot_config/bin/adhoc/executable_qemu_guest_agent.sh
@@ -20,7 +20,7 @@ fi
 
 if ! systemctl is-active --quiet qemu-guest-agent; then
     echo_with_color "$GREEN_COLOR" "Starting qemu-guest-agent..."
-    $SYSTEMCTL start qemu-guest-agent || exit_with_error "Failed to start qemu-guest-agent"
+    sudo systemctl start qemu-guest-agent || exit_with_error "Failed to start qemu-guest-agent"
     echo_with_color "$GREEN_COLOR" "qemu-guest-agent is installed and running"
     echo_with_color "$GREEN_COLOR" "If just installed then you may need to restart the VM for the changes to take effect"
 else
