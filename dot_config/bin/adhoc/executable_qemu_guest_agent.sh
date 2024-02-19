c0dc732011792fef4758a6ae0d1833e5c4d6f539
Tim Bryant <timothybryant3@gmail.com>
2024-02-19 12:29:32 -0500

    chore: add else block with echo info

--- dot_config/bin/adhoc/executable_qemu_guest_agent.sh
+++ dot_config/bin/adhoc/executable_qemu_guest_agent.sh
@@ -16,6 +16,8 @@ fi
 if ! systemctl is-enabled --quiet qemu-guest-agent; then
     echo_with_color "$GREEN_COLOR" "Enabling qemu-guest-agent..."
     sudo systemctl enable qemu-guest-agent || exit_with_error "Failed to enable qemu-guest-agent"
+    echo_with_color "$GREEN_COLOR" "If just installed then restart the VM and start qemu-guest-agent"
+    exit 0
 else
     echo_with_color "$GREEN_COLOR" "qemu-guest-agent is already enabled"
 fi
@@ -24,7 +26,6 @@ if ! systemctl is-active --quiet qemu-guest-agent; then
     echo_with_color "$GREEN_COLOR" "Starting qemu-guest-agent..."
     sudo systemctl start qemu-guest-agent || exit_with_error "Failed to start qemu-guest-agent"
     echo_with_color "$GREEN_COLOR" "qemu-guest-agent is installed and running"
-    echo_with_color "$GREEN_COLOR" "If just installed then you may need to restart the VM for the changes to take effect"
 else
     echo_with_color "$GREEN_COLOR" "qemu-guest-agent is already running"
 fi
