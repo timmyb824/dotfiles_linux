f51f832b93bf8ebbc041250ce004a2b3755bbbb0
Tim Bryant <timothy.bryant@simplisafe.com>
2024-03-26 12:24:08 -0400

    refactor: podman script

--- dot_config/bin/utilities/executable_podman_delete_service_unit.sh
+++ dot_config/bin/utilities/executable_podman_delete_service_unit.sh
@@ -8,24 +8,22 @@ fi
 
 # Service name is the first argument
 SERVICE_NAME="$1"
+UNIT_FILE="container-$SERVICE_NAME.service"
 
 # Stop the user service
-systemctl --user stop "$SERVICE_NAME"
+systemctl --user stop "$UNIT_FILE"
 
 # Disable the user service
-systemctl --user disable "$SERVICE_NAME"
+systemctl --user disable "$UNIT_FILE"
 
 # Remove the user service file
-rm -f "$HOME/.config/systemd/user/${SERVICE_NAME}.service"
+rm -f "$HOME/.config/systemd/user/$UNIT_FILE"
 
 # Reload the daemon in case the file removal needs to be recognized
 systemctl --user daemon-reload
 
-# Extract the container name from the service name
-CONTAINER_NAME=$(echo "$SERVICE_NAME" | sed -r 's/^container-(.*)\.service/\1/')
-
 # Stop and remove the container
-podman stop "$CONTAINER_NAME"
-podman rm "$CONTAINER_NAME"
+podman stop "$SERVICE_NAME"
+podman rm "$SERVICE_NAME"
 
 echo "Service and container for $SERVICE_NAME have been removed."
