107955584b936b7bc4eb386f43105eb3732cb052
Tim Bryant <timothy.bryant@simplisafe.com>
2024-03-28 11:03:42 -0400

    fix: Update installation steps for 1Password CLI in the shell script

--- dot_config/bin/installers/1password.sh
+++ dot_config/bin/installers/1password.sh
@@ -4,20 +4,17 @@ source "$(dirname "$BASH_SOURCE")/../init/init.sh"
 
 install_op_cli_linux() {
     # Install the 1Password CLI using the new steps provided
-    if ! sudo -s -- <<EOF
-curl -sS https://downloads.1password.com/linux/keys/1password.asc | \
-gpg --dearmor --output /usr/share/keyrings/1password-archive-keyring.gpg
+    if ! sudo sh -c 'curl -sS https://downloads.1password.com/linux/keys/1password.asc | \
+gpg --dearmor --output /usr/share/keyrings/1password-archive-keyring.gpg && \
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/1password-archive-keyring.gpg] https://downloads.1password.com/linux/debian/$(dpkg --print-architecture) stable main" | \
-tee /etc/apt/sources.list.d/1password.list
-mkdir -p /etc/debsig/policies/AC2D62742012EA22/
+tee /etc/apt/sources.list.d/1password.list && \
+mkdir -p /etc/debsig/policies/AC2D62742012EA22/ && \
 curl -sS https://downloads.1password.com/linux/debian/debsig/1password.pol | \
-tee /etc/debsig/policies/AC2D62742012EA22/1password.pol
-mkdir -p /usr/share/debsig/keyrings/AC2D62742012EA22
+tee /etc/debsig/policies/AC2D62742012EA22/1password.pol && \
+mkdir -p /usr/share/debsig/keyrings/AC2D62742012EA22 && \
 curl -sS https://downloads.1password.com/linux/keys/1password.asc | \
-gpg --dearmor --output /usr/share/debsig/keyrings/AC2D62742012EA22/debsig.gpg
-apt update && apt install -y 1password-cli
-EOF
-    then
+gpg --dearmor --output /usr/share/debsig/keyrings/AC2D62742012EA22/debsig.gpg && \
+apt update && apt install -y 1password-cli'; then
         echo_with_color "$RED_COLOR" "Error: The 1Password CLI installation failed."
         return 1
     fi
