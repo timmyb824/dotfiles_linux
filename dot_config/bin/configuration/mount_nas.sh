be8bd9273d2318301f50d7607d793051d8cce05b
Tim Bryant <timothybryant3@gmail.com>
2024-03-30 11:30:42 -0400

    refactor: Simplify and improve conditional logic in mount_nas.sh

--- dot_config/bin/configuration/mount_nas.sh
+++ dot_config/bin/configuration/mount_nas.sh
@@ -11,10 +11,9 @@ EXPECTED_USER="$USER"
 echo_with_color "$BLUE_COLOR" "UPDATE SHARED FOLDER RULE WITH IP OF THE VM IN THE SYNOLOGY NAS OR THIS WILL FAIL."
 
 # Check if we can cd into the directory or if it is not empty
-if [ -d "$MOUNT_POINT" ]; then
-    if ! cd "$MOUNT_POINT" || [ "$(ls -A "$MOUNT_POINT")" ]; then
-        exit_with_error "$MOUNT_POINT cannot be accessed or is not empty."
-    fi
+if [ -d "$MOUNT_POINT" ] && cd "$MOUNT_POINT" && [ -z "$(ls -A "$MOUNT_POINT")" ]; then
+    echo_with_color "$BLUE_COLOR" "$MOUNT_POINT is already mounted and accessible."
+    exit 0
 fi
 
 echo_with_color "$BLUE_COLOR" "Installing nfs-common if not already installed..."
@@ -39,18 +38,10 @@ if ! sudo mount -t nfs -o vers=4 "$NAS_IP:$NAS_SHARE" "$MOUNT_POINT"; then
 fi
 echo_with_color "$GREEN_COLOR" "Successfully mounted the NFS share $NAS_SHARE from $NAS_IP."
 
-# Check if the expected user has access to the directory after mounting
-if [ "$(stat -c '%U' "$MOUNT_POINT")" != "$EXPECTED_USER" ]; then
-    echo_with_color "$BLUE_COLOR" "Changing ownership of $MOUNT_POINT to $EXPECTED_USER..."
-    if ! sudo chown "$EXPECTED_USER:$EXPECTED_USER" "$MOUNT_POINT"; then
-        exit_with_error "Failed to change ownership of $MOUNT_POINT."
-    fi
-    echo_with_color "$GREEN_COLOR" "Successfully changed ownership of $MOUNT_POINT."
-fi
 
 echo_with_color "$BLUE_COLOR" "Setting permissions for $MOUNT_POINT..."
-# Only change the permissions of the folder itself, not recursively
-if ! sudo chmod 755 "$MOUNT_POINT"; then
+# Only change the permissions of the folders
+if ! sudo find "$MOUNT_POINT" -type d -exec chown "$EXPECTED_USER:$EXPECTED_USER" {} +; then
     exit_with_error "Failed to set permissions for $MOUNT_POINT."
 fi
 echo_with_color "$GREEN_COLOR" "Successfully set permissions for $MOUNT_POINT."
