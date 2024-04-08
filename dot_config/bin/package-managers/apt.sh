b34b1f39f5e968e47bfe281c833217c61a5ff248
Tim Bryant <timothybryant3@gmail.com>
2024-04-08 16:57:07 -0400

    feat: Add script to install apt packages in bash script

---
+++ dot_config/bin/package-managers/apt.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+source "$(dirname "$BASH_SOURCE")/../init/init.sh"
+
+install_apt_packages() {
+    echo_with_color "$CYAN_COLOR" "Installing apt packages..."
+
+    while IFS= read -r package; do
+        trimmed_package=$(echo "$package" | xargs)  # Trim whitespace from the package name
+        if [ -n "$trimmed_package" ]; then  # Ensure the line is not empty
+            if sudo apt install -y "$trimmed_package"; then
+                echo_with_color "$GREEN_COLOR" "${trimmed_package} installed successfully"
+            else
+                exit_with_error "Failed to install ${trimmed_package}"
+            fi
+        fi
+    done < <(get_package_list apt.list)
+
+    echo_with_color "$GREEN_COLOR" "All apt packages installed successfully."
+}
+
+# Ensure apt is installed
+if ! command_exists apt; then
+    echo_with_color "$RED_COLOR" "apt could not be found"
+
+    exit_with_error "apt is required to install packages. Please install apt to continue."
+fi
+
+install_apt_packages
