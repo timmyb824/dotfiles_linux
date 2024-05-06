08127dbe591ddb084032e8a12e7a7723679e5191
Tim Bryant <timothybryant3@gmail.com>
2024-05-05 23:08:51 -0400

    chore: Add fzf configuration to zsh packages file

--- dot_config/zsh/packages.zsh
+++ dot_config/zsh/packages.zsh
@@ -7,6 +7,9 @@ if command -v pyenv 1>/dev/null 2>&1; then
     eval "$(pyenv virtualenv-init -)"
 fi
 
+# --- fzf ---
+[ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
+
 
 # --- other ---
 eval "$(zoxide init --cmd cd zsh)"
