28bf32ce1a8ccbb9835e735cdc782200b16e87cb
Tim Bryant <timothybryant3@gmail.com>
2024-05-05 23:24:55 -0400

    feat: Add functions to install and uninstall fzf, check for git installation

--- dot_config/bin/installers/fzf.sh
+++ dot_config/bin/installers/fzf.sh
@@ -2,15 +2,19 @@
 
 source "$(dirname "$BASH_SOURCE")/../init/init.sh"
 
-# Check if git is installed
-if ! command_exists git; then
-    exit_with_error "git is not installed - please install git and run this script again"
-fi
 
-# TODO: Add support for updating fzf
-# Check if basher is not already installed
-if [ ! -d "$HOME/.fzf" ]; then
-    echo "fzf is not installed. Installing now..."
+uninstall_fzf_apt() {
+    if [ -f /usr/bin/fzf ]; then
+        echo_with_color "$YELLOW_COLOR" "fzf is already installed at /usr/bin/fzf"
+        echo_with_color "$YELLOW_COLOR" "Uninstalling fzf"
+        sudo apt remove fzf -y || exit_with_error "Failed to uninstall fzf"
+        echo_with_color "$YELLOW_COLOR" "fzf uninstalled successfully"
+    else
+        echo_with_color "$YELLOW_COLOR" "fzf is not installed at /usr/bin/fzf"
+    fi
+}
+
+install_fzf() {
     if git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf; then
         echo_with_color "$GREEN_COLOR" "fzf cloned successfully; running installer"
         if ~/.fzf/install; then
@@ -22,6 +26,13 @@ if [ ! -d "$HOME/.fzf" ]; then
     else
         exit_with_error "Failed to clone fzf"
     fi
-else
-    echo_with_color "$BLUE_COLOR" "fzf is already installed at $HOME/.fzf"
+}
+
+# Check if git is installed
+if ! command_exists git; then
+    exit_with_error "git is not installed - please install git and run this script again"
 fi
+
+uninstall_fzf_apt
+install_fzf
+
