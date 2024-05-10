8856561a6660b7da82934b41364097917930b2cb
Tim Bryant <timothybryant3@gmail.com>
2024-05-10 13:11:59 -0400

    style: Simplify keybindings names in which-key plugin

--- dot_config/nvim/lua/tbryant/plugins/which-key.lua
+++ dot_config/nvim/lua/tbryant/plugins/which-key.lua
@@ -11,22 +11,22 @@ return {
               name = "+code"
           },
           ["<leader>e"] = {
-              name = "+file explorer"
+              name = "+explorer"
           },
           ["<leader>f"] = {
-              name = "+fuzzy find"
+              name = "+fuzzy"
           },
           ["<leader>m"] = {
-              name = "+format visual mode"
+              name = "+format"
           },
           ["<leader>n"] = {
               name = "+clear search"
           },
           ["<leader>s"] = {
-              name = "+split windows"
+              name = "+windows"
           },
           ["<leader>t"] = {
-              name = "+tab management"
+              name = "+tabs"
           },
           ["<leader>w"] = {
               name = "+session"
