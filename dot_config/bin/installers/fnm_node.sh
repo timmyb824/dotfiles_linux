3eb0c68dc99eb61a705ae189f82329cb1ed4cb2d
Tim Bryant <timothybryant3@gmail.com>
2024-05-19 15:01:08 -0400

    feat(installers): Add initialization function for cargo in fnm_node.sh

--- dot_config/bin/installers/fnm_node.sh
+++ dot_config/bin/installers/fnm_node.sh
@@ -2,18 +2,35 @@
 
 source "$(dirname "$BASH_SOURCE")/../init/init.sh"
 
+# Function to initialize cargo
+initialize_cargo() {
+    if command_exists cargo; then
+        echo_with_color "$GREEN_COLOR" "cargo is already installed."
+    else
+        echo_with_color "$YELLOW_COLOR" "Initializing cargo..."
+        if [ -f "$HOME/.cargo/env" ]; then
+            source "$HOME/.cargo/env"
+        else
+            echo_with_color "$RED_COLOR" "Cargo environment file does not exist."
+            exit_with_error "Please install cargo to continue." 1
+        fi
+    fi
+}
+
 # Function to initialize fnm for the current session
 initialize_fnm_for_session() {
     # Initialize fnm without specifying a shell
     eval "$(fnm env --use-on-cd)"
 }
 
+
 # Check if npm is installed and working
 if ! command_exists npm; then
     echo_with_color "$RED_COLOR" "npm could not be found."
 
     # Attempt to fix fnm command availability
-    attempt_fix_command fnm "$HOME/.local/bin"
+    initialize_cargo
+    initialize_fnm_for_session
 
     # Check for fnm again
     if command_exists fnm; then
