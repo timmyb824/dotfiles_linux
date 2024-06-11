aff7a57743d85ac214df39c78bc2b33fa67d8493
Tim Bryant <timothybryant3@gmail.com>
2024-06-11 18:39:31 -0400

    fix: Update Nebula binary version to v1.9.3 and use sudo for moving to /usr/local/bin

--- dot_config/bin/installers/nebula.sh
+++ dot_config/bin/installers/nebula.sh
@@ -5,10 +5,10 @@ source "$(dirname "$BASH_SOURCE")/../init/init.sh"
 # Function to download and extract Nebula binaries
 download_and_extract_nebula() {
   echo "Downloading Nebula..."
-  curl -LO https://github.com/slackhq/nebula/releases/download/v1.5.2/nebula-linux-amd64.tar.gz
+  curl -LO https://github.com/slackhq/nebula/releases/download/v1.9.3/nebula-linux-amd64.tar.gz
   tar -xzf nebula-linux-amd64.tar.gz
-  mv nebula /usr/local/bin/
-  mv nebula-cert /usr/local/bin/
+  sudo mv nebula /usr/local/bin/
+  sudo mv nebula-cert /usr/local/bin/
 }
 
 # Function to set up the Lighthouse
