ef0744612a5f70fa9c6ff2ad2aeded8505a9f5e7
Tim Bryant <timothybryant3@gmail.com>
2024-06-12 02:12:00 -0400

    feat: Improve Nebula installation script with optimized file downloads and configuration updates

--- dot_config/bin/installers/nebula.sh
+++ dot_config/bin/installers/nebula.sh
@@ -5,7 +5,7 @@ source "$(dirname "$BASH_SOURCE")/../init/init.sh"
 # Function to download and extract Nebula binaries
 download_and_extract_nebula() {
   printf "Downloading Nebula...\n"
-  curl -LO https://github.com/slackhq/nebula/releases/download/v1.9.3/nebula-linux-amd64.tar.gz
+  curl -LOq https://github.com/slackhq/nebula/releases/download/v1.9.3/nebula-linux-amd64.tar.gz
   tar -xzf nebula-linux-amd64.tar.gz
   sudo mv nebula /usr/local/bin/
   sudo mv nebula-cert /usr/local/bin/
@@ -27,9 +27,11 @@ setup_lighthouse() {
   sudo mkdir -p /etc/nebula
 
   # Download and configure example config
-  curl -o /tmp/config-lighthouse.yaml https://raw.githubusercontent.com/slackhq/nebula/master/examples/config.yml
+  curl -oq /tmp/config-lighthouse.yaml https://raw.githubusercontent.com/slackhq/nebula/master/examples/config.yml
   sed -i 's/# am_lighthouse: false/am_lighthouse: true/' /tmp/config-lighthouse.yaml
   sed -i "s/#static_host_map:/static_host_map:\n  '$lh_ip': ['$lh_routable_ip:4242']/" /tmp/config-lighthouse.yaml
+  sed -i '/inbound:/a \  inbound:\n    - port: any\n      proto: any\n      host: any' /tmp/config.yaml
+
 
   # Move certificates and config to config directory
   sudo mv ca.crt /etc/nebula/
@@ -37,8 +39,22 @@ setup_lighthouse() {
   sudo mv "$lh_name".key /etc/nebula/host.key
   sudo mv /tmp/config-lighthouse.yaml /etc/nebula/config.yaml
 
+  if command_exists zellij; then
+    printf "Starting Nebula with Zellij...\n"
+    zellij -s nebula -c "sudo /usr/local/bin/nebula -config /etc/nebula/config.yaml"
+  elif command_exists screen; then
+    printf "Starting Nebula with Screen...\n"
+    screen -dmS nebula sudo /usr/local/bin/nebula -config /etc/nebula/config.yaml
+  elif command_exists tmux; then
+    printf "Starting Nebula with Tmux...\n"
+    tmux new-session -d -s nebula sudo /usr/local/bin/nebula -config /etc/nebula/config.yaml
+  else
+    printf "Starting Nebula...\n"
+    sudo /usr/local/bin/nebula -config /etc/nebula/config.yaml
+  fi
+
   # Start Nebula
-  sudo /usr/local/bin/nebula -config /etc/nebula/config.yaml
+  # sudo /usr/local/bin/nebula -config /etc/nebula/config.yaml
 }
 
 # Function to set up a regular host
@@ -57,10 +73,11 @@ setup_host() {
   sudo mkdir -p /etc/nebula
 
   # Download and configure example config
-  curl -o /tmp/config.yaml https://raw.githubusercontent.com/slackhq/nebula/master/examples/config.yml
+  curl -oq /tmp/config.yaml https://raw.githubusercontent.com/slackhq/nebula/master/examples/config.yml
   sed -i "s/#static_host_map:/static_host_map:\n  '192.168.100.1': ['$lh_routable_ip:4242']/" /tmp/config.yaml
   sed -i 's/# am_lighthouse: false/am_lighthouse: false/' /tmp/config.yaml
   sed -i "s/# hosts:/hosts:\n    - '192.168.100.1'/" /tmp/config.yaml
+  sed -i '/inbound:/a \  inbound:\n    - port: any\n      proto: any\n      host: any' /tmp/config.yaml
 
   # Move certificates and config to config directory
   sudo mv ca.crt /etc/nebula/
@@ -68,8 +85,22 @@ setup_host() {
   sudo mv "$host_name".key /etc/nebula/host.key
   sudo mv /tmp/config.yaml /etc/nebula/config.yaml
 
-  # Start Nebula
-  sudo /usr/local/bin/nebula -config /etc/nebula/config.yaml
+  if command_exists zellij; then
+    printf "Starting Nebula with Zellij...\n"
+    zellij -s nebula -c "sudo /usr/local/bin/nebula -config /etc/nebula/config.yaml"
+  elif command_exists screen; then
+    printf "Starting Nebula with Screen...\n"
+    screen -dmS nebula sudo /usr/local/bin/nebula -config /etc/nebula/config.yaml
+  elif command_exists tmux; then
+    printf "Starting Nebula with Tmux...\n"
+    tmux new-session -d -s nebula sudo /usr/local/bin/nebula -config /etc/nebula/config.yaml
+  else
+    printf "Starting Nebula...\n"
+    sudo /usr/local/bin/nebula -config /etc/nebula/config.yaml
+  fi
+
+  # # Start Nebula
+  # sudo /usr/local/bin/nebula -config /etc/nebula/config.yaml
 }
 
 # Main script logic
@@ -86,7 +117,7 @@ main() {
   # Create CA certificate if it does not exist
   if [ ! -f ca.crt ]; then
     printf "Creating Certificate Authority...\n"
-    nebula-cert ca -name "Myorganization, Inc"
+    nebula-cert ca -name "BryantHomelab"
   fi
 
   # Run appropriate setup function
