a25845efe97343c14b4d250ce9d39c63a2c8b747
Tim Bryant <timothybryant3@gmail.com>
2024-06-17 13:10:24 -0400

    feat: Add promtail user to systemd-journal group

--- dot_config/bin/installers/promtail.sh
+++ dot_config/bin/installers/promtail.sh
@@ -48,6 +48,7 @@ create_promtail_user() {
 add_promtail_to_adm_group() {
     echo_with_color "$GREEN" "Adding promtail user to the adm group..."
     sudo usermod -aG adm promtail || echo_with_color "$RED" "Failed to add promtail user to the adm group."
+    sudo usermod -aG systemd-journal promtail || echo_with_color "$RED" "Failed to add promtail user to the systemd-journal group."
     echo_with_color "$GREEN" "Added promtail user to the adm group."
 }
 
