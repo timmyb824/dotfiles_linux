2bee793dc4db5129a9b75b330b6d0bf473a9a41e
Tim Bryant <timothybryant3@gmail.com>
2024-06-21 14:27:38 -0400

    feat: Add function to install dependencies and build podman_exporter

--- dot_config/bin/installers/podman_exporter.sh
+++ dot_config/bin/installers/podman_exporter.sh
@@ -6,12 +6,20 @@ SERVICE_FILE="/etc/systemd/system/podman_exporter.service"
 REPO_LOCATION="$HOME/DEV/podman_exporter"
 USER=$CURRENT_USER
 
+install_dependencies() {
+    echo_with_color "$GREEN_COLOR" "Installing dependencies..."
+    sudo apt install -y btrfs-progs-devel device-mapper-devel gpgme-devel libassuan-devel || exit_with_error "Failed to install dependencies."
+}
+
 clone_podman_exporter() {
     echo_with_color "$GREEN_COLOR" "Creating directory for podman_exporter..."
     mkdir -p "$REPO_LOCATION" || exit_with_error "Failed to create directory: $REPO_LOCATION"
     echo_with_color "$GREEN_COLOR" "Cloning podman_exporter..."
     git clone https://github.com/containers/prometheus-podman-exporter.git "$REPO_LOCATION" || exit_with_error "Failed to clone podman_exporter."
-    chmod +x "$REPO_LOCATION/bin/prometheus-podman-exporter" || exit_with_error "Failed to make prometheus-podman-exporter executable."
+
+    echo_with_color "$GREEN_COLOR" "Building podman_exporter; this may take a while..."
+    cd "$REPO_LOCATION" || exit_with_error "Failed to change directory to $REPO_LOCATION"
+    make binary || exit_with_error "Failed to build podman_exporter."
 }
 
 # Function to create systemd service file
@@ -56,6 +64,7 @@ main() {
         exit_with_error "Git is not installed. Please install Git first."
     fi
 
+    install_dependencies
     clone_podman_exporter
     create_systemd_service_file
     start_podman_exporter_service
