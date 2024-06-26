87d53d45003fab665c2480f89c6ac419e0e29102
Tim Bryant <timothybryant3@gmail.com>
2024-06-26 10:28:33 -0400

    fix: Update path to oci_autocomplete.sh for Python 3.11

--- dot_config/zsh/completions.zsh
+++ dot_config/zsh/completions.zsh
@@ -2,5 +2,4 @@ if command -v ngrok &>/dev/null; then
   eval "$(ngrok completion)"
 fi
 
-[[ -e "/home/tbryant/oracle-cli/lib/python3.10/site-packages/oci_cli/bin/oci_autocomplete.sh" ]] && source "/home/tbryant/oracle-cl
-i/lib/python3.10/site-packages/oci_cli/bin/oci_autocomplete.sh"
+[[ -e "/home/tbryant/oracle-cli/lib/python3.11/site-packages/oci_cli/bin/oci_autocomplete.sh" ]] && source "/home/tbryant/oracle-cli/lib/python3.11/site-packages/oci_cli/bin/oci_autocomplete.sh"
