680cf4d41c2de96dd6c78af37e2f63dfd6395cfb
Tim Bryant <timothybryant3@gmail.com>
2024-06-26 10:23:01 -0400

    style: Fix formatting in shell scripts for OCI CLI installation and Zsh completions

--- dot_config/zsh/completions.zsh
+++ dot_config/zsh/completions.zsh
@@ -1,3 +1,6 @@
 if command -v ngrok &>/dev/null; then
   eval "$(ngrok completion)"
 fi
+
+[[ -e "/home/tbryant/oracle-cli/lib/python3.10/site-packages/oci_cli/bin/oci_autocomplete.sh" ]] && source "/home/tbryant/oracle-cl
+i/lib/python3.10/site-packages/oci_cli/bin/oci_autocomplete.sh"
