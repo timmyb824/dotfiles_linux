4b7c7b2cc8db633634dd188da8d1c7a4de8b5193
Tim Bryant <timothybryant3@gmail.com>
2024-07-02 13:00:31 -0400

    fix: Correct adding and removing nameservers in executable_cloudflare_dns script

--- dot_config/bin/utilities/executable_cloudflare_dns.sh
+++ dot_config/bin/utilities/executable_cloudflare_dns.sh
@@ -6,16 +6,17 @@ if [ -z "$1" ]; then
   exit 1
 fi
 
-# if arg is `add` then append 1.1.1.1 to top of resolv.conf
+# if arg is `add` then prepend 1.1.1.1 to top of resolv.conf
 if [ "$1" == "add" ]; then
   echo "Adding 1.1.1.1 to resolv.conf..."
-  echo "nameserver 1.1.1.1" | sudo tee /etc/resolv.conf >/dev/null
+  # Create a temporary file with the new nameserver at the top
+  sudo sed -i '1inameserver 1.1.1.1' /etc/resolv.conf
   echo "Done"
 fi
 
 # if arg is `remove` then remove 1.1.1.1 from resolv.conf
 if [ "$1" == "remove" ]; then
   echo "Removing 1.1.1.1 from resolv.conf..."
-  sed -i '1d' /etc/resolv.conf
+  sudo sed -i '/^nameserver 1.1.1.1$/d' /etc/resolv.conf
   echo "Done"
 fi
